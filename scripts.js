33e93023344a3d55957075f6c08056e3b6b26dfd
Tiara <tburtin1019@gmail.com>
2023-02-16 15:14:52 -0600

    updated buttons with cool stuff

--- scripts.js
+++ scripts.js
@@ -1,2 +1,18 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", function() {
+    const paragraph = document.getElementById('flightStatus')
+    const colorInBack = document.getElementById('shuttleBackground')
+    const height = document.getElementById('spaceShuttleHeight')
+    const button = document.getElementById('takeoff')
+    button.addEventListener("click", function() {
+        let response = 
+        window.confirm('Confirm that the shuttle is ready for takeoff.')
+        if (response) { 
+            paragraph.innerHTML = 'Shuttle in flight'
+            colorInBack.style.backgroundColor = 'blue'
+            height.innerHTML = 10000
+        }
+    })
+
+})  
